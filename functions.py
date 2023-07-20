addb218b7e4eab6a100dc639778f8654ec338a12
pradivgnanaraj <tpradivgnanaraj@gmail.com>
2023-07-20 10:23:50 +0530

    Inital Commit

---
+++ functions.py
@@ -0,0 +1,27 @@
+FILEPATH = "todos.txt"
+
+def get_todos(filepath=FILEPATH):
+    """
+    This function is used as a content manager.
+    Returns a text file and return the list of to-do item(s)
+    :return:
+    """
+    with open(filepath, 'r') as file_local:
+        todos_local = file_local.readlines()
+    return todos_local
+
+
+def write_todos(todos_arg, filepath=FILEPATH):
+    """
+    This function is used to write data to the file
+    :return:
+    """
+    with open(filepath, 'w') as file:
+        file.writelines(todos_arg)
+
+
+if __name__ == "__cli__":
+    print("Hello")
+    print(get_todos())
+
+# delete this line after git
